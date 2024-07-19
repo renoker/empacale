c131aaac00a737bfea515aebb62323ac7ef9ffff
renoker <rodolfoulises.ramirez@gmail.com>
2024-07-19 11:10:39 -0600

    update

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/sass/main.scss',
                 'resources/js/app.js',
                 'resources/js/codigo_lote.js',
                 'resources/js/home.js',
