d6ff3d202a82066b3c4638669d6fee2bc41cdf7a
renoker <rodolfoulises.ramirez@gmail.com>
2024-07-20 08:15:56 -0600

    Update Mobile

--- database/seeders/WeekSeeder.php
+++ database/seeders/WeekSeeder.php
@@ -13,28 +13,29 @@ class WeekSeeder extends Seeder
      */
     public function run(): void
     {
-        Week::create([
-            'name'          => 'Semana 1',
-            'start_date'    => '2024-07-15 00:00:00',
-            'end_date'      => '2024-07-21 23:59:59',
-        ]);
 
         Week::create([
-            'name'          => 'Semana 3',
-            'start_date'    => '2024-07-22 00:00:00',
+            'name'          => 'Semana 1',
+            'start_date'    => '2024-07-20 00:00:00',
             'end_date'      => '2024-07-28 23:59:59',
         ]);
 
         Week::create([
-            'name'          => 'Semana 3',
+            'name'          => 'Semana 2',
             'start_date'    => '2024-07-29 00:00:00',
             'end_date'      => '2024-08-04 23:59:59',
         ]);
 
         Week::create([
-            'name'          => 'Semana 4',
+            'name'          => 'Semana 3',
             'start_date'    => '2024-08-05 00:00:00',
             'end_date'      => '2024-08-11 23:59:59',
+        ]);
+
+        Week::create([
+            'name'          => 'Semana 4',
+            'start_date'    => '2024-08-12 00:00:00',
+            'end_date'      => '2024-08-18 23:59:59',
         ]);
     }
 }
