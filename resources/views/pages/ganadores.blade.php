28a8d11c759d5d8f6e4dbd428afaf2587932748c
renoker <rodolfoulises.ramirez@gmail.com>
2024-08-21 16:44:07 -0600

    Update

--- resources/views/pages/ganadores.blade.php
+++ resources/views/pages/ganadores.blade.php
@@ -4,9 +4,9 @@
 
 @section('content')
     <div class="content_ganadores">
-        <img src="{{ url('assets/images/ganadores/playa.png') }}" class="img_playa" alt="">
-        <img src="{{ url('assets/images/ganadores/pueblo_magico.png') }}" alt="">
-        <img src="{{ url('assets/images/ganadores/bosque.png') }}" alt="">
-        <img src="{{ url('assets/images/ganadores/ciudad.png') }}" alt="">
+        <img src="{{ url('assets/images/ganadores/Ganadores_Playa.png') }}" class="img_playa" alt="">
+        <img src="{{ url('assets/images/ganadores/Ganadores_PuebloMagico.png') }}" alt="">
+        <img src="{{ url('assets/images/ganadores/Ganadores_Bosque.png') }}" alt="">
+        <img src="{{ url('assets/images/ganadores/Ganadores_Ciudad.png') }}" alt="">
     </div>
 @endsection
